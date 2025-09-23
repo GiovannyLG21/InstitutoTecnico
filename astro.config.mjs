b7d6fc44bb9f86e420b3bb6b58af2bf954e23817
GiovannyLG <ladigiococ@gmail.com>
2025-09-23 05:54:57 -0500

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [react()],
+
   env: {
     schema: {
       WEB_URL: envField.string({ access: 'public', context: 'client' })
     }
-  }
+  },
+
+  adapter: vercel()
 });
